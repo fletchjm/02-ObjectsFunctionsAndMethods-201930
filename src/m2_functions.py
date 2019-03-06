bd47b1da9476c218a2e3a4658e142dddcc680718
fletchjm <fletchjm@rose-hulman.edu>
2019-03-06 14:06:00 -0500

    DONE

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -29,7 +29,7 @@ def main():
     answer = get_hypot(3, 4)
     print(answer)
 
-    get_turtles('red', 3)
+    get_turtles('red', 10)
 
 
 
@@ -51,7 +51,7 @@ def get_hypot(side1, side2):
 
 
 ###############################################################################
-# TODO: 4a.  Define a function immediately below this _TODO_.
+# DONE: 4a.  Define a function immediately below this _TODO_.
 #   It takes two arguments:
 #     -- a string that represents a color (e.g. 'red')
 #     -- a positive integer that represents the thickness of a Pen.
@@ -73,7 +73,7 @@ def get_hypot(side1, side2):
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 4b.  In main, CALL your function at least TWICE (with different values
+# DONE: 4b.  In main, CALL your function at least TWICE (with different values
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
@@ -94,7 +94,7 @@ def get_turtles(color, thick):
     window.close_on_mouse_click()
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
